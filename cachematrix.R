d2cff9eb616b3705a7a61bf6a0a7a21944e38329
Arthur Etchells <aetchells@imaging-resource.com>
2015-09-23 22:00:43 -0400

    commit of incremental progress for matrix cache code

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get function() x
+  getInverse <- function(inv)
+  setInverse <- function(solve) inv <<- solve
+  
+  return(
+    list(
+      set=set,
+      get=get,
+      getInverse=getInverse,
+      setInverse=setInverse
+      )
+    )
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse
+  if(!is.null(m)) {
+    message("getting inverted matrix")
+    return(inverse)
+  }
+  matrix <- x$get()
+  inverse <- solve(matrix)
+  x.setInverse(inverse)
+  return(inverse)
 }
